6fee732c90f075827e5b4cb26d84f6f4b22972df
Cntrfct <zef.frz@gmail.com>
2023-10-25 19:05:25 +0400

    add docker

--- bot/bot.py
+++ bot/bot.py
@@ -9,13 +9,14 @@ from keyboards.main_menu import set_main_menu
 # Инициализируем логер
 logger = logging.getLogger(__name__)
 
+
 # Функция конфигурирования и запуска бота
 async def main():
     # Конфигурация логирование
     logging.basicConfig(
         level=logging.INFO,
         format='%(filename)s:%(lineno)d #%(levelname)-8s '
-                '[%(asctime)s] - %(name)s - %(message)s')
+               '[%(asctime)s] - %(name)s - %(message)s')
 
     # Выводим в консоль информацию о начале запуска бота
     logger.info('Starting bot. . .')
@@ -38,6 +39,6 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+
 if __name__ == '__main__':
     asyncio.run(main())
-    
